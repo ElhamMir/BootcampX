ba40123fc595dd749cf619618066dfe1c5cd0b5b
Elham <mireshghi@yahoo.com>
2021-11-25 23:31:45 +0000

    bootcamp_app

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -6,12 +6,17 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
+WHERE cohorts.name LIKE '%${cohortName}%'
+LIMIT ${limit};
 `)
 .then(res => {
   res.rows.forEach(user => {
